5f85ff33b59b939f35f3e9c0f1bfc90f6a34c06b
Brian <bn22m@pm.me>
2021-04-14 18:32:45 +0200

    make it better

--- myappds/myappds/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="myappds.Global_asax" Language="vb" %>
--- myappds/myappds/Index.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Index
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        cldInfo.SelectedDate = Now.Date
-    End Sub
-
-End Class
